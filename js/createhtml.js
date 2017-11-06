fe8a3c1ab957ce52698c5a7c0f3765e62df7576c
DiamondCrack <zk05161219@gmail.com>
2017-11-06 20:43:06 +0800

    update js-createHtml

--- js/createhtml.js
+++ js/createhtml.js
@@ -1,59 +1,87 @@
 
+const vari ={
 
-createChildren(dataBase,0);
-clickFile(dataBase);
+    conRow:document.querySelector('.container .row'),
+    container:document.querySelector('.container'),
+    breadCrumbNav:document.querySelector('.bread-crumb'),
+    currentId:0
+};
 
-function createChildren(db,id){
-    let children = getChildrenById(db,id);
-    let fileStr = '';
-    vari.conRow.innerHTML = '';
-    for(let i=0; i<children.length;i++){
+//生成文件夹节点
+function createFileNode(fileData){
 	let file = document.createElement('div');
 	file.className = 'col-1';
-	vari.conRow.appendChild(file);
-	 fileStr =`<div class="file-con">
+	 file.innerHTML =`<div class="file-con">
                                <i class="true"></i>
                         <div class="file">
                             <div class="grey-file"></div>
                             <div class="main-file"></div>
                         </div>
-                        <div class="file-title">${children[i].name}</div>
+                        <div class="file-title">${fileData.name}</div>
                         <div class="rename">
-                            <input name="" type="text" value="${children[i].name}" />
+                            <input name="" type="text" value="${fileData.name}" />
                             <div class="yes"></div>
                             <div class="no"></div>
                         </div>
                     </div>
                     `;
-	file.innerHTML += fileStr;
-	file.fileId = children[i].id;
-    }
-        if(!fileStr) {
-	vari.conRow.innerHTML = '没有文件';
-	return;
-    }
+
+    let fileItems = file.querySelectorAll('div');
+	for(let i=0; i<fileItems.length; i++){
+	    fileItems[i].fileId = fileData.id;
+	}
+    console.log(fileItems[0].fileld);
+    return file;
 }
 
+//生成面包屑导航节点
+function createBreadCrumbNode(fileData){
+    const crumbList = document.createElement('li');
+    const href = document.createElement('a');
 
-function clickFile(db){
-    const cols =  document.querySelectorAll('.row .col-1');
-    //console.log(cols);
-    const breadCrumbNav = document.querySelector('.bread-crumb');
-    for(let i=0; i<cols.length; i++){
-	cols[i].addEventListener('click',function(){
-	    thisCrumb = document.createElement('li');
-	    breadCrumbNav.appendChild(thisCrumb);
-	    thisCrumb.innerHTML = `>${db[this.fileId].name}`;
-	    thisCrumb.fileId = this.fileId;
-	createChildren(db,this.fileId);
-	clickFile(db);
+    href.fileId = fileData.id;
+    href.innerHTML = `<i>&gt;</i> ${fileData.name}`;
+    href.href = 'javascript:;';
+    crumbList.appendChild(href);
+    return crumbList;
+}
+
+function createFileList(db,id){
+    vari.conRow.innerHTML = '';
+    let children = getChildrenById(db,id);
+
+    children.forEach(function(item,i){
+	vari.conRow.appendChild(createFileNode(item));
     });
 }
+
+
+function createBreadCrumb(db,id){
+    vari.breadCrumbNav.innerHTML = '';
+    var data = getAllParen(db,id);
+    data.forEach(function(item,i){
+	vari.breadCrumbNav.appendChild(createBreadCrumbNode(item));
+    });
+    
+}
+let currentId =0;
+openFile(dataBase,vari.currentId);
+
+function openFile(db,currentId){
+    createFileList(db,currentId);
+    createBreadCrumb(db,currentId);
 }
 
-function clickCrumb(){
+vari.conRow.addEventListener('click',function(e){
+    const target = e.target;
+    if(target.classList.contains('main-file') || target.classList.contains('grey-file') || target.classList.contains('file-title')){
+	openFile(dataBase,vari.currentId = target.fileId);
+    }
+});
 
-}
-
-
-
+vari.breadCrumbNav.addEventListener('click',function(e){
+    const target = e.target;
+    if(target.fileId !==undefined && vari.cuurentId !== target.fileId){
+	openFile(dataBase,vari.currentId = target.fileId);
+    }
+});
--- js/data.js
+++ js/data.js
@@ -1,5 +1,5 @@
 dataBase={
-    '0':{id:0,        name:'根目录'},
+    '0':{id:0,        name:'全部文件'},
     '1':{id:1, pId:0, name:'电影'},
     '2':{id:2, pId:0, name:'音乐'},
     '3':{id:3, pId:2, name:'古典音乐'},
